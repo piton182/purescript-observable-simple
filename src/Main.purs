1f38ee1eda0a9731b481cdc9898cc9e9000d6405
Dmitry Kouznetsov <dmitry.kouznetsov@viaplay.com>
2017-07-03 18:14:14 +0200

    subscribeNext instead of subscribe

--- src/Main.purs
+++ src/Main.purs
@@ -3,15 +3,11 @@ module Main where
 import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
-import Control.Monad.Eff.Exception (Error)
-import RxJS.Observable (range, subscribe, Observable)
+import RxJS.Observable (range, subscribeNext)
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = subscribe subscriber numbers
-  where
-    numbers = range 1 10
-    subscriber =
-      { next: log <<< append "Next: " <<< show
-      , error: log <<< append "Error: " <<< show
-      , completed: const $ log "Completed."
-      }
+main = do
+  _ <- subscribeNext (log <<< append "Next: " <<< show) numbers
+  pure unit
+    where
+      numbers = range 1 10
